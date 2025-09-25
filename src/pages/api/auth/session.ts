5017aa1b8fd42cc4522cd663a95bc93863bef400
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2025-09-24 22:39:33 -0400

    more ts

--- src/pages/api/auth/session.ts
+++ src/pages/api/auth/session.ts
@@ -1,6 +1,7 @@
 import { auth0 } from "@/lib/auth0";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await auth0.getSession(req);
   res.status(200).json(session || {});
 }
