b468edab2020b5f91b0c20ddc0e9efd84c3ba4d2
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2026-02-07 20:45:21 -0500

    cleanup and comments

--- src/app/api/points/route.ts
+++ src/app/api/points/route.ts
@@ -34,22 +34,24 @@ export async function GET(request: Request) {
       }
     })
 
-    // flip to [lat, lng] for leaflet
-      const grouped: Record<string, { positions: [number, number][], color: string }> = {};
+    // empty hashmap object to prepare for grouping by shapeid, adding position and colors as values
+    const grouped: Record<string, { positions: [number, number][], color: string }> = {};
     
     points.forEach(point => {
+      // if key of shape id not in hashmap, create it with empty positions, add color
       if (!grouped[point.shapeId]) {
         grouped[point.shapeId] = {
           positions: [],
           color: point.line.color
         };
       }
+      //push positions for the current shape id
       grouped[point.shapeId].positions.push([point.shapePointLatitude, point.shapePointLongitude]);
     });
     return NextResponse.json(grouped);
 
   } catch (error) {
-console.error("Error fetching line shapes:", error);
+    console.error("Error fetching line shapes:", error);
     return NextResponse.json(
       { error: "Failed to fetch line shapes" },
       { status: 500 }
--- src/components/MapLine.tsx
+++ src/components/MapLine.tsx
@@ -16,21 +16,29 @@ L.Icon.Default.mergeOptions({
 
 export default function MapLine() {
   const [points, setPoints] = useState<Record<string, { positions: [number, number][], color: string }>>({});
+  // this creates a hashmap for line ids as keys, positions and the line color as values, will look like {
+//   "5..N01R": { 
+//     positions: [[40.7, -73.9], [40.71, -73.91], ...], 
+//     color: "#009952" 
+//   },
+//   "5..N32R": { 
+//     positions: [[40.8, -73.8], ...], 
+//     color: "#009952" 
+//   }
+// }
 
-  // const lineId = line.id;
-useEffect(() => {
+  useEffect(() => {
     // all lines
     fetch(`/api/lines`)
       .then(res => res.json())
       .then((data: Line[]) => {
-        console.log("got lines: ", data);
         
         // shapes for each line
         data.forEach((line: Line) => {
           fetch(`/api/points?lineId=${line.id}`)
             .then(res => res.json())
             .then((shapes: Record<string, { positions: [number, number][], color: string }>) => {
-              
+              // takes response from api in correct hashmap format, takes previous state and appends new line shape
               setPoints(prev => ({
                 ...prev,
                 ...shapes  
@@ -49,13 +57,13 @@ useEffect(() => {
     
   return (
     <div style={{ height: '500px', width: '100%' }}>
-<MapContainer center={[40.7128, -74.0060]} zoom={12} style={{ height: '100%', width: '100%' }}>
-  <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      <MapContainer center={[40.7128, -74.0060]} zoom={12} style={{ height: '100%', width: '100%' }}>
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'>
 
-  </TileLayer>
+        </TileLayer>
 
-{Object.entries(points).map(([shapeId, { positions, color }]) => {
+        {Object.entries(points).map(([shapeId, { positions, color }]) => {
           return positions && positions.length > 0 ? (
             <Polyline 
               key={shapeId}
@@ -68,7 +76,7 @@ useEffect(() => {
             />
           ) : null;
         })}
-</MapContainer>
+      </MapContainer>
     </div>
   )
 }
